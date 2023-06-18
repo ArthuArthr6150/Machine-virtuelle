c097dff6ff26de62540eaa70343ad86d5a9f6072
ArthuArthr6150 <128979530+ArthuArthr6150@users.noreply.github.com>
2023-06-18 15:46:51 +0200

    V1.1

--- Calculator.py
+++ Calculator.py
@@ -7,15 +7,18 @@ Division(à écrire tel quel)?
     while True:
         try:
             num1 = int(input("Le premier nombre : "))
+            print()
             while True:
                 try:
                     num2 = int(input("Le deuxième nombre : "))
+                    print()
                     break
                 except ValueError as e:
                     print("Il faut que se soit un nombre entier.")
             break
         except ValueError as e:
             print("Il faut que se soit un nombre entier.")
+    print()
     if opé == "Addition":
         print(f"La réponse : {num1 + num2}")
     elif opé == "Soustraction":
@@ -23,6 +26,8 @@ Division(à écrire tel quel)?
     elif opé == "Multiplication":
         print(f"La réponse : {num1 * num2}")
     elif opé == "Division":
-        print(f"La réponse : {num1 / num2}")
+        if num2 == 0:
+            print("On ne peut pas avoir le dividende à zéro.")
+        else: print(f"La réponse : {num1 / num2}")
     else:
         print("Ce n'est pas une solution possible.")
