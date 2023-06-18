704455dc042f80ff93e361d698502619a68f7c0f
ArthuArthr6150 <128979530+ArthuArthr6150@users.noreply.github.com>
2023-06-18 16:27:41 +0200

    V2.0

--- Déverouille.py
+++ Déverouille.py
@@ -1,10 +1,15 @@
 from getpass import getpass
+import hashlib
 
-def Verrouillage(code):
+def Verrouillage(code_hash):
     dév = False
     while not dév:
         ask = getpass("Donne moi le mot de passe : ")
-        if ask != code:
+        sha = hashlib.sha256()
+        ask_byte = ask.encode('utf-8')
+        sha.update(ask_byte)
+        ask_hache = sha.hexdigest()
+        if ask_hache != code_hash:
             print("Mot de passe incorecte.")
         else:  
             dév = True
