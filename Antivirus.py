28d0b1fe25674d696a1f6fbf985e0be82720aa5c
ArthuArthr6150 <128979530+ArthuArthr6150@users.noreply.github.com>
2023-06-24 09:09:44 +0200

    V2.0

--- Antivirus.py
+++ Antivirus.py
@@ -2,11 +2,14 @@ import json
 import time
 
 
-def AntiV():
+def AntiV(user):
     numO = 0
     with open('Fichier.json', 'r') as file:
         Fichier = json.load(file)
-    for num in range(len(Fichier)):
-        numO += 1
-        print(f"{numO}/{len(Fichier)}")
+    for num in range(len(Fichier[user])):
         time.sleep(1)
+        numO += 1
+        print()
+        print(f"{numO}/{len(Fichier[user])}")
+    print()
+    print("0 virus, 0 malwaire, 0 piratage")
