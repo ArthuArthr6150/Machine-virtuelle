78b42478cc16a2ea61c07905d1832364f4968f23
ArthuArthr6150 <128979530+ArthuArthr6150@users.noreply.github.com>
2024-11-07 11:00:43 +0100

    V2.9.9.1

--- Antivirus.py
+++ Antivirus.py
@@ -1,15 +1,14 @@
-import json
+import os
 import time
 
 
 def AntiV(user):
+    fichiers = os.listdir(f"Fichiers/{user}")
     numO = 0
-    with open('Fichier.json', 'r') as file:
-        Fichier = json.load(file)
-    for num in range(len(Fichier[user])):
+    for num in range(len(os.listdir)):
         time.sleep(1)
         numO += 1
         print()
-        print(f"{numO}/{len(Fichier[user])}")
+        print(f"{numO}/{len(os.listdir)}")
     print()
     print("0 virus, 0 malwaire, 0 piratage")
